e63f4aaac1132fd0033288e8d26003b9b74be4fd
Raz Israeli <razisraeli1@gmail.com>
2023-12-14 11:58:19 +0200

    1.8-Structuring angular unit tests - test setup using beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,24 +3,28 @@ import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
 
-  it('should add two numbers', () => {
-    // const logger = new LoggerService(); // This is a real instance of the logger service.
-    const logger = jasmine.createSpyObj('LoggerService', ['log']); //? This is a mock instance of the logger service with the name of the function we want the spy to use.
-    // spyOn(logger, 'log') // spy on the instance of logger, specifically on the times the function "log" is called. This is not needed when we was jasmine.createSpyObj().
+  //! we can't define the variables inside the "beforeEach" scope since then the "it" blocks will not be able to use them, so they are created here.
+  let calculator: CalculatorService;
+  let loggerSpy: any; //This is of type any since this is a jasmine spy and NOT AN INSTANCE OF THE LOGGER SERVICE.
 
-    const calculator = new CalculatorService(logger);
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  });
+
+  it('should add two numbers', () => {
 
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4, "unexpected addition result!");
-    expect(logger.log).toHaveBeenCalledTimes(1); //how many times that function can be called (otherwise the test fails)
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
 
     const result = calculator.subtract(7, 5);
 
     expect(result).toEqual(2);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
