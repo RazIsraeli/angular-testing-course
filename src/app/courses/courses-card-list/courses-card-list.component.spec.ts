19ded058464c94a6fb33a0b05ddfdffaf26cc190
Raz Israeli <razisraeli1@gmail.com>
2023-12-15 15:37:53 +0200

    3.20-angular component testing - test setup with the waitForAsync test utility

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { CoursesCardListComponent } from './courses-card-list.component';
 import { CoursesModule } from '../courses.module';
 import { COURSES } from '../../../../server/db-data';
@@ -11,17 +11,27 @@ import { AppComponent } from '../../app.component';
 
 describe('CoursesCardListComponent', () => {
 
-  beforeEach(() => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>; // needed in order to create an instance of a component.
+
+
+  beforeEach(waitForAsync(() => { // WaitForAsync is angular's test utility function that receives another function. It know that there are async processes in the provided function, and it waits for them to end before moving on to the next specification.
     TestBed.configureTestingModule({
       imports: [
         CoursesModule,
       ]
-    });
-  });
+    })
+      .compileComponents()
+      .then(() => { // In this block we will setup the test, meaning, we will populate the global variables that were declared inside the "describe" block.
+        fixture = TestBed.createComponent(CoursesCardListComponent); //
+        component = fixture.componentInstance; // Creating an instance of the component we want to test.
+      });
+  }));
 
 
   it('should create the component', () => {
-
+    console.log("🚀 ~ file: courses-card-list.component.spec.ts:35 ~ it ~ component:", component)
+    expect(component).toBeTruthy('No component was created');
   });
 
 });
